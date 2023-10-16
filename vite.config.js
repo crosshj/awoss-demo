4b19441895476d73ecf6c5c2f9fa24a87852f4f6
crosshj <github@chimpjuice.com>
2023-10-16 01:23:57 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import vercel from "vite-plugin-vercel";
+//import vercel from "vite-plugin-vercel";
 
 // polyfill node
 // https://gist.github.com/FbN/0e651105937c8000f10fefdf9ec9af3d
